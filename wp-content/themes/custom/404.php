f318c012d67970f46916df0a77fd6db96b5151d5
Teddy Bradford <teddy@fastmail.com>
2017-06-28 13:56:43 -0400

    Remove WordPress files

--- wp-content/themes/custom/404.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$context = Timber::get_context();
-
-$templates = array("404.twig");
-
-Timber::render($templates, $context);
--- wp-content/themes/custom/sidebar.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$context = array();
-$context["widgets"] = Timber::get_widgets("dynamic_sidebar");
-
-$templates = array("sidebar.twig");
-
-Timber::render($templates, $context);
