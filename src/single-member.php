2e59ed098189773568698869a3e2421b8ddd6f1d
Teddy Bradford <teddy@fastmail.com>
2017-07-07 18:33:53 -0400

    Creates separate templates for company and professional member roles

--- src/single-member.php
+++ src/single-member.php
@@ -1,15 +1,15 @@
 <?php
 
 global $params;
-global $wp_roles;
 
 $context = Timber::get_context();
+
 $member = new Timber\User($params["member_id"]);
+$member_role = key($member->wp_capabilities);
 
 $context["member"] = $member;
 $context["wp_title"] = $member->name;
 
-$member_role = key($member->wp_capabilities);
-$context["member_category"] = $wp_roles->roles[$member_role]["name"]; // Role display name
+$templates = array("single-member-$member_role.twig", "single-member.twig");
 
-Timber::render("single-member.twig", $context);
+Timber::render($templates, $context);
