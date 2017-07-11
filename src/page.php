f2917aac21f401bd3011b1d0bfbb8832a9800d83
Teddy Bradford <teddy@fastmail.com>
2017-07-11 15:43:15 -0400

    Rename $post to $page

--- src/page.php
+++ src/page.php
@@ -1,14 +1,14 @@
 <?php
 
 $context = Timber::get_context();
-$post = Timber::get_post();
+$page = Timber::get_post();
 
-$context["post"] = $post;
+$context["page"] = $page;
 
 if (is_front_page()) {
   $context["posts"] = Timber::get_posts("post_type=post");
 }
 
-$templates = array("page-$post->post_name.twig", "page.twig");
+$templates = array("page-$page->post_name.twig", "page.twig");
 
 Timber::render($templates, $context);
