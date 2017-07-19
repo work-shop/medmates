b0220aa31cd479c38fa1d30674fe0463287c92e7
Teddy Bradford <teddy@fastmail.com>
2017-07-19 15:00:28 -0400

    Use pretty search URLs for global search

--- src/search.php
+++ src/search.php
@@ -1,8 +1,18 @@
 <?php
 
+if ($_GET) {
+  $s = get_query_var("s");
+
+  if ($s != "") {
+    wp_redirect(str_replace("%20", "+", esc_url(site_url("/search/$s"))));
+  } else {
+    wp_redirect(site_url($base_path));
+  }
+  exit();
+}
+
 $context = Timber::get_context();
 $search_query = get_search_query();
-
 
 $context["wp_title"] = ($search_query) ? "Search Results for “" . $search_query . "”" : "Search";
 
