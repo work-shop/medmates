18841b6931a01e5e8bfea0712992df2afbc15ac1
Teddy Bradford <teddy@fastmail.com>
2017-07-25 14:42:31 -0400

    Show taxonomy description for resource categories

--- src/taxonomy.php
+++ src/taxonomy.php
@@ -3,9 +3,11 @@
 $context = Timber::get_context();
 $templates = array("taxonomy.twig", "archive.twig");
 
-$taxonomy = get_query_var("taxonomy");
-array_unshift($templates, "taxonomy-$taxonomy.twig");
+$term = get_queried_object();
+
+array_unshift($templates, "taxonomy-$term->taxonomy.twig");
 
 $context["wp_title"] = single_cat_title("", false);
+$context["description"] = term_description($term->term_id, $term->taxonomy);
 
 Timber::render($templates, $context);
