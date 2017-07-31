25c28614fee54c1e84e6b5107bb079d7381b79dd
Teddy Bradford <teddy@fastmail.com>
2017-07-31 12:25:33 -0400

    Add edit profile link on profile page if user is logged in

--- src/member.php
+++ src/member.php
@@ -3,11 +3,14 @@
 global $params;
 
 $context = Timber::get_context();
-$member = new Timber\User($params["member_id"]);
+
+$member_id = $params["member_id"];
+$member = new Timber\User($member_id);
 $member->role = key($member->wp_capabilities);
 
 $context["post"] = $member;
 $context["wp_title"] = $member->name;
+$context["member_id"] = $member_id;
 
 $templates = array("member-$member->role.twig", "member.twig");
 
